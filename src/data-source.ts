974ad436c8f646f53f1636b9c4247d84c5ce3bc5
Marcela <mamadipaula@gmail.com>
2023-04-12 10:51:36 -0300

    deploy

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,29 +1,45 @@
-import { DataSource } from "typeorm"
+import { DataSource, DataSourceOptions } from "typeorm"
 import path from "path"
 import "dotenv/config"
 import { Anuncio } from "./entities/advertisement.entity"
 import { createAnuncio1681241216367 } from "./migrations/1681241216367-createAnuncio"
 
-const AppDataSource = new DataSource(
-    process.env.NODE_ENV === "test" ?
-    {
-        type: "sqlite",
-        database: ":memory:",
-        synchronize: true,
-        entities: ["src/entities/*.ts"]
-    } :
-    {
+const setDataSourceConfig = (): DataSourceOptions => {
+  
+    const nodeEnv = process.env.NODE_ENV
+
+    if(nodeEnv === "production"){
+        return {
+            type: "postgres",
+            url: process.env.DATABASE_URL,
+            entities: [Anuncio],
+            migrations: [createAnuncio1681241216367],
+        }
+    }
+
+    if(nodeEnv === "test"){
+        return {
+            type: "sqlite",
+            database: ":memory:",
+            synchronize: true,
+            entities: [Anuncio],
+        }
+    }
+
+    return {
         type: "postgres",
-        host: process.env.PGHOST!,
+        host: process.env.PGHOST,
         port: parseInt(process.env.PGPORT!),
-        username: process.env.PGUSER!,
-        password: process.env.PGPASSWORD!,
-        database: process.env.PGDATABASE!,
+        username: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        database: process.env.PGDATABASE,
         logging: true,
         synchronize: false,
         entities: [Anuncio],
         migrations: [createAnuncio1681241216367]
     }
-)
+}
 
-export default AppDataSource
+const dataSourceConfig = setDataSourceConfig()
+
+export default new DataSource(dataSourceConfig)
