5b76cae2c9b034fd6e9f39e827f87ab0d2740c0f
Lucas Cruz <lucascruz0103@gmail.com>
2023-04-20 09:34:15 -0300

    acertando os erros na rota

--- src/routes/advertisement.routes.ts
+++ src/routes/advertisement.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { createAdvertisementController, getAllAdvertisementsController, getAdvertisementController, deleteAdvertisementController, updateAdvertisementController } from "../controller/advertisement.controller"
+import { createAdvertisementController, getAllAdvertisementsController, getAdvertisementController, deleteAdvertisementController, updateAdvertisementController, listAllWithUsersController } from "../controller/advertisement.controller"
 import ensureDataIsValidMiddleware from "../middlewares/ensureDataIsValid.middleware"
 import { createAdvertisementSerializer } from "../serializers/advertisement.serializer"
 
@@ -16,4 +16,4 @@ advertisementRoutes.delete("/:id", deleteAdvertisementController)
 
 advertisementRoutes.patch("/:id", updateAdvertisementController)
 
-advertisementRoutes.get("/users/:id", updateAdvertisementController)
+advertisementRoutes.get("/users/:id", listAllWithUsersController)
