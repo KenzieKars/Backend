02280ce8c283001be1e9973e406d3b6e068b5718
felipebravo <felipe_bravo@hotmail.com>
2023-04-28 17:56:32 -0300

    feat: remove image and password field from edit user schema

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,48 +1,47 @@
-import { DataSource, DataSourceOptions } from "typeorm"
-import path from "path"
-import "dotenv/config"
-import { Anuncio } from "./entities/advertisement.entity"
-import { User } from "./entities/user.entity"
-import { Endereco } from "./entities/address.entity"
-import { Migration1682692226716 } from "./migrations/1682692226716-migration"
-
+import { DataSource, DataSourceOptions } from 'typeorm';
+import path from 'path';
+import 'dotenv/config';
+import { Anuncio } from './entities/advertisement.entity';
+import { User } from './entities/user.entity';
+import { Endereco } from './entities/address.entity';
+// import { Migration1682692226716 } from "./migrations/1682692226716-migration"
+import { Migration1682712223454 } from './migrations/1682712223454-migration';
 
 const setDataSourceConfig = (): DataSourceOptions => {
-  
-    const nodeEnv = process.env.NODE_ENV
+	const nodeEnv = process.env.NODE_ENV;
 
-    if(nodeEnv === "production"){
-        return {
-            type: "postgres",
-            url: process.env.DATABASE_URL,
-            entities: [Anuncio,User,Endereco],
-            migrations: [Migration1682692226716],
-        }
-    }
+	if (nodeEnv === 'production') {
+		return {
+			type: 'postgres',
+			url: process.env.DATABASE_URL,
+			entities: [Anuncio, User, Endereco],
+			migrations: [Migration1682712223454],
+		};
+	}
 
-    if(nodeEnv === "test"){
-        return {
-            type: "sqlite",
-            database: ":memory:",
-            synchronize: true,
-            entities: [Anuncio],
-        }
-    }
+	if (nodeEnv === 'test') {
+		return {
+			type: 'sqlite',
+			database: ':memory:',
+			synchronize: true,
+			entities: [Anuncio],
+		};
+	}
 
-    return {
-        type: "postgres",
-        host: process.env.PGHOST,
-        port: parseInt(process.env.PGPORT!),
-        username: process.env.PGUSER,
-        password: process.env.PGPASSWORD,
-        database: process.env.PGDATABASE,
-        logging: true,
-        synchronize: false,
-        entities: [Anuncio,User,Endereco],
-        migrations: [Migration1682692226716]
-    }
-}
+	return {
+		type: 'postgres',
+		host: process.env.PGHOST,
+		port: parseInt(process.env.PGPORT!),
+		username: process.env.PGUSER,
+		password: process.env.PGPASSWORD,
+		database: process.env.PGDATABASE,
+		logging: true,
+		synchronize: false,
+		entities: [Anuncio, User, Endereco],
+		migrations: [Migration1682712223454],
+	};
+};
 
-const dataSourceConfig = setDataSourceConfig()
+const dataSourceConfig = setDataSourceConfig();
 
-export default new DataSource(dataSourceConfig)
+export default new DataSource(dataSourceConfig);
