23bb72d48583823ce4bf5fae467efd51269dec9b
wesCarv <wcarvalho516@gmail.com>
2023-05-08 09:47:43 -0300

    feat: docs complete with swagger

--- src/app.ts
+++ src/app.ts
@@ -10,11 +10,13 @@ import loginRoutes from "./routes/login.routes"
 import senhaRoutes from "./routes/senha.routes"
 import addressRoutes from "./routes/address.routes"
 import { commentsRoutes } from "./routes/comments.routes"
-
+import swaggerUi from "swagger-ui-express"
+import swaggerDocs from "./swagger.json"
 
 const app = express()
 app.use(express.json())
 app.use(pagination)
+app.use("/api-docs" , swaggerUi.serve , swaggerUi.setup(swaggerDocs))
 
 app.use(
     cors({
